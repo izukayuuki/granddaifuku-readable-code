4b1f8cbbe4301927e5f986f7e7e50a791816a19a
granddaifuku <daifukuformac@gmail.com>
2021-09-13 16:13:36 +0900

    file open

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 
+#define BUFFER_SIZE 256
+
 int main() {
-    printf("上手\n");
+    FILE *fp;
+    char data[BUFFER_SIZE];
+    char scanword[BUFFER_SIZE];
+    if ((fp = fopen(data, "r")) == NULL) {
+        printf("ファイルオープン失敗\n");
+    } else {
+        fscanf(fp, "%s", scanword);
+        printf("%s", scanword);
+    }
+    fclose(fp);
+    
     return 0;
 }
