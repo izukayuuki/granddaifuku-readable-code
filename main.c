0bc0af26b38873e6344442488fa6ff5fd4d30047
granddaifuku <daifukuformac@gmail.com>
2021-09-14 13:23:54 +0900

    仕様5の実装

--- main.c
+++ main.c
@@ -6,13 +6,16 @@ int main() {
     FILE *fp;
     char data[BUFFER_SIZE];
     char scanword[BUFFER_SIZE];
+    int ID = 1;
+
     printf("ファイル名：");
     scanf("%s", data);
     if ((fp = fopen(data, "r")) == NULL) {
         printf("ファイルオープン失敗\n");
     } else {
         while (fscanf(fp, "%s", scanword) != EOF) {
-            printf("%s\n", scanword);
+            printf("%d: %s\n", ID, scanword);
+            ID++;
         }
     }
     fclose(fp);
