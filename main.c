624584ef859cbd7ec17992d5e5768d605cd3a103
granddaifuku <daifukuformac@gmail.com>
2021-09-14 13:13:13 +0900

    仕様3の実装

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 
-#define BUFFER_SIZE 256
+const int BUFFER_SIZE = 256;
 
 int main() {
     FILE *fp;
     char data[BUFFER_SIZE];
     char scanword[BUFFER_SIZE];
-    //ここでファイルの入力をしていない
+    scanf("ファイル名：%s", data);
     if ((fp = fopen(data, "r")) == NULL) {
         printf("ファイルオープン失敗\n");
     } else {
         fscanf(fp, "%s", scanword);
-        printf("%s", scanword);
+        printf("%s\n", scanword);
     }
     fclose(fp);
     
