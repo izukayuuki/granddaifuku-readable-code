f03fd69807358fe38776b015710a734577b57396
granddaifuku <daifukuformac@gmail.com>
2021-09-14 13:30:52 +0900

    仕様6の実装

--- main.c
+++ main.c
@@ -1,21 +1,35 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 const int BUFFER_SIZE = 256;
 
-int main() {
+int main(int argc, char* argv[]) {
     FILE *fp;
     char data[BUFFER_SIZE];
     char scanword[BUFFER_SIZE];
     int ID = 1;
-
+    char searchID[BUFFER_SIZE];
+    int convertID;
+    
     printf("ファイル名：");
     scanf("%s", data);
+    fgets(searchID, BUFFER_SIZE, stdin);
+    convertID = atoi(searchID);
     if ((fp = fopen(data, "r")) == NULL) {
         printf("ファイルオープン失敗\n");
     } else {
-        while (fscanf(fp, "%s", scanword) != EOF) {
+        if (convertID == 0) {
+            while (fscanf(fp, "%s", scanword) != EOF) {
             printf("%d: %s\n", ID, scanword);
             ID++;
+            }
+        } else {
+            while (fscanf(fp, "%s", scanword) != EOF) {
+                if(ID == convertID){
+                printf("%d: %s\n", ID, scanword);
+                }
+                ID++;
+            }
         }
     }
     fclose(fp);
