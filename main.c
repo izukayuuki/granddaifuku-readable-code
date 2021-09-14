cd1147cd85b49390af607e4045d31f6810bbd28e
izukayuuki <izukayuuki1030@gmail.com>
2021-09-14 15:17:39 +0900

    コメントを追加

--- main.c
+++ main.c
@@ -9,17 +9,19 @@ int main(int argc, char* argv[]) {
     char scanword[BUFFER_SIZE];
     
     int ID = 1;
-    char searchID[BUFFER_SIZE];
-    int convertID;
+    char searchID[BUFFER_SIZE]; //探したいID番号
+    int convertID; //型変換したあと
 
     printf("ファイル名 ID番号の形式で入力してください：");
     scanf("%s", data);
     fgets(searchID, BUFFER_SIZE, stdin);
-    convertID = atoi(searchID);
+    convertID = atoi(searchID); //char型からint型に変換
     
+    //ファイルを開けたかのif文
     if ((fp = fopen(data, "r")) == NULL) {
         printf("ファイルオープン失敗\n");
     } else {
+        //ファイルオープンが成功した時の処理
         if (convertID == 0) {
             while (fscanf(fp, "%s", scanword) != EOF) {
                 printf("%d: %s\n", ID, scanword);
